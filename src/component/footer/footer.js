f4bcfd12860faabd8df10a24dec853756ef180a2
Roman Khemych <roman19940912@gmail.com>
2025-02-10 18:36:58 +0200

    Change home, phone, mail icon

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -1,9 +1,6 @@
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PhoneAndroidOutlinedIcon from "@mui/icons-material/PhoneAndroidOutlined";
-import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
-import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
+import { HomeIcon } from "../../icon/home";
+import { PhoneIcon } from "../../icon/phone";
+import { MailIcon } from "../../icon/mail";
 import "./footer.scss";
 
 function Footer() {
@@ -14,7 +11,7 @@ function Footer() {
           <div className="grid grid--tablet">
             <div className="grid__item grid__item--t--1-2 grid__item--d--1-4">
               <div className="footer__widget-container">
-                <HomeOutlinedIcon className="footer__widget-icon" />
+                <HomeIcon className="footer__widget-icon" />
                 <h4 className="footer__widget-title">Address</h4>
                 <h4 className="footer__widget-subtitle">
                   We cover: PA, NJ, DE
@@ -23,7 +20,7 @@ function Footer() {
             </div>
             <div className="grid__item grid__item--t--3-4 grid__item--d--5-8">
               <div className="footer__widget-container">
-                <PhoneAndroidOutlinedIcon className="footer__widget-icon" />
+                <PhoneIcon className="footer__widget-icon" />
                 <h4 className="footer__widget-title">Phone</h4>
                 <h4 className="footer__widget-subtitle">
                   Phone: +1 267 6162 999
@@ -32,7 +29,7 @@ function Footer() {
             </div>
             <div className="grid__item grid__item--t--5-6 grid__item--d--9-12">
               <div className="footer__widget-container">
-                <EmailOutlinedIcon className="footer__widget-icon" />
+                <MailIcon className="footer__widget-icon" />
                 <h4 className="footer__widget-title">Email</h4>
                 <h4 className="footer__widget-subtitle">
                   rkstonerepair@gmail.com
